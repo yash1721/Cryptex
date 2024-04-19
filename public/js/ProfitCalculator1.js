836be0a76aa40d0a07d25178c8b50ecb4c490afd
Madhav Agrawal <madhavagrawal012@gmail.com>
2024-04-19 16:00:40 +0530

    added complete frontend and remaining backend

---
+++ public/js/ProfitCalculator1.js
@@ -0,0 +1,70 @@
+document.getElementById('other-coin').style.display = 'none';
+
+document.getElementById('crypto-select').addEventListener('change', function () {
+  if (document.getElementById('crypto-select').value == 'OTHER') {
+    document.getElementById('other-coin').style.display = 'block';
+  } else {
+    document.getElementById('other-coin').style.display = 'none';
+  }
+});
+
+document.getElementById('calculate').addEventListener('click', function (event) {
+  event.preventDefault();
+  document.getElementById("loader").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
+  const unit = document.getElementById('amount').value; // Added .value to get the input value
+  const url = '/api/predict';
+  const coinData = {
+    coinName: document.getElementById('crypto-select').value == 'OTHER' ? document.getElementById('other-coin').value.toLowerCase() : document.getElementById('crypto-select').value,
+    timeStamp: `${document.getElementById('exit-date').value} 00:00:00`
+  };
+  const option = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(coinData),
+  };
+
+  fetch(url, option)
+    .then(response => response.json())
+    .then(data => {
+      if (data.success) {
+        var selling_price = parseFloat(data.message.slice(1, -1));
+        var current_price = '';
+        const url = `https://api.coingecko.com/api/v3/simple/price?ids=${document.getElementById('crypto-select').value}&vs_currencies=usd`;
+        const option = {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          mode: 'cors',
+        };
+        console.log("Hey")
+        fetch(url, option)
+          .then(response => response.json())
+          .then(cdata => {
+            current_price = cdata[document.getElementById('crypto-select').value == 'OTHER' ? document.getElementById('other-coin').value.toLowerCase() : document.getElementById('crypto-select').value].usd;
+            current_price = parseFloat(current_price);
+            
+          })
+          .then(error => {
+            console.log(error);
+          });
+        document.getElementById("loader").style.display = "none";
+        document.getElementById("overlay").style.display = "none";
+        document.getElementById('profit-heading').textContent = 'Total Profit Gained'
+        document.getElementById('profit').textContent = '$' + (selling_price - current_price) * unit; // Assuming unit is a number
+
+      } else {
+        document.getElementById('Profit').textContent = data.message;
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+document.getElementById('SelectMarket-price').addEventListener('change', function () {
+  document.getElementById('buy-price').disabled = document.getElementById('SelectMarket-price').checked;
+});
